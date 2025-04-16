6ce45fcc379e405620d25d82ec36a0530f8c43f2
Dan G <daniel.w.gray1994@gmail.com>
2025-04-16 09:18:22 -0400

    modularized google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
-import { Bebas_Neue, Inter } from "next/font/google";
 import "./globals.css";
-
-const bebasNeue = Bebas_Neue({ weight: "400", subsets: ["latin"] });
-
-const inter = Inter({ subsets: ["latin"] });
+import { inter } from "@/lib/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bebasNeue.className} ${inter.className} antialiased`}>
-        {children}
-      </body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ src/lib/fonts.ts
@@ -0,0 +1,5 @@
+import { Bebas_Neue, Inter } from "next/font/google";
+
+export const bebasNeue = Bebas_Neue({ weight: "400", subsets: ["latin"] });
+
+export const inter = Inter({ subsets: ["latin"] });
