5c6a095a24f8ffd7a67fca5de4643d7a2b22859b
Dan G <daniel.w.gray1994@gmail.com>
2025-04-16 09:58:39 -0400

    added toggle to open and close the navigation-panel

--- src/components/navigation-panel.tsx
+++ src/components/navigation-panel.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { useState } from "react";
+
 export default function NavigationPanel() {
+  const [open, setOpen] = useState(true);
+
   return (
-    <aside className="fixed top-0 left-0 h-full w-1/4 bg-foreground transform translate-x-0">
-      NavigationPanel
+    <aside
+      className={`flex flex-col fixed top-0 left-0 h-full w-1/5 bg-foreground p-2 transform transition-transform duration-300 ${
+        open ? "translate-x-0" : "-translate-x-4/5"
+      }`}
+    >
+      <button className="ml-auto" onClick={() => setOpen(!open)}>
+        {open ? "Close" : "Menu"}
+      </button>
     </aside>
   );
 }
