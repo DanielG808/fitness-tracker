1528f38227ec0c0363a1f8abab25aebbe3a63404
Dan G <daniel.w.gray1994@gmail.com>
2025-04-17 08:56:11 -0400

    created navLinks array and mapped it inside nav-link-list

--- src/components/nav-link-list.tsx
+++ src/components/nav-link-list.tsx
@@ -1,13 +1,45 @@
 import NavLink from "./nav-link";
-import { CalendarIcon } from "@heroicons/react/16/solid";
+import {
+  CalendarIcon,
+  FireIcon,
+  IdentificationIcon,
+  Cog8ToothIcon,
+} from "@heroicons/react/16/solid";
+
+const navLinks = [
+  {
+    icon: <CalendarIcon className="w-8 h-8" />,
+    name: "Workout Calendar",
+    path: "/",
+  },
+  {
+    icon: <FireIcon className="w-8 h-8" />,
+    name: "Workouts",
+    path: "/workouts",
+  },
+  {
+    icon: <IdentificationIcon className="w-8 h-8" />,
+    name: "Profile",
+    path: "/profile",
+  },
+  {
+    icon: <Cog8ToothIcon className="w-8 h-8" />,
+    name: "Settings",
+    path: "/settings",
+  },
+];
 
 export default function NavLinkList() {
   return (
-    <nav className="flex flex-col m-20 space-y-10">
-        <NavLink />
-        <NavLink />
-        <NavLink />
-        <NavLink />
+    <nav>
+      <ul className="flex flex-col m-20 space-y-10">
+        {navLinks.map(({ icon, name, path }) => (
+          <li key={path} className="flex items-center">
+            {icon}
+            <a href={path}>{name}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
-  )
+  );
 }
