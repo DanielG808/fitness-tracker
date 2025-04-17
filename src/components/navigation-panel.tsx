df8ddd2083c16be38d3a9a984174150e62c1f15d
Dan G <daniel.w.gray1994@gmail.com>
2025-04-17 09:41:55 -0400

    added logo to navigation-panel

--- src/components/navigation-panel.tsx
+++ src/components/navigation-panel.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { XMarkIcon, Bars3Icon } from "@heroicons/react/16/solid";
 import NavLinkList from "./nav-link-list";
+import Logo from "./logo";
 
 export default function NavigationPanel() {
   const [open, setOpen] = useState(true);
@@ -15,13 +16,16 @@ export default function NavigationPanel() {
           : "-translate-x-5/6 shadow-[4px_0_10px_rgba(0,0,0,0.4)]"
       }`}
     >
-      <button className="ml-auto" onClick={() => setOpen(!open)}>
-        {open ? (
-          <XMarkIcon className="w-8 h-8" />
-        ) : (
-          <Bars3Icon className="w-10 h-10" />
-        )}
-      </button>
+      <div className="flex items-center">
+        <Logo />
+        <button className="ml-auto" onClick={() => setOpen(!open)}>
+          {open ? (
+            <XMarkIcon className="w-8 h-8" />
+          ) : (
+            <Bars3Icon className="w-10 h-10" />
+          )}
+        </button>
+      </div>
       <NavLinkList />
     </aside>
   );
