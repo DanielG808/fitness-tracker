5df543630ea55c65251997896a2fdd168fc9de98
Dan Gray <dan.gray@nice.com>
2025-04-21 18:56:14 -0400

    added test case for nav link styling

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -1,4 +1,5 @@
 import test, { expect } from "playwright/test";
+import { formatRgb, oklch } from "culori"
 import { navLinks } from "@/lib/constants/navLinks";
 
 test.describe("Navigation Panel", () => {
@@ -18,5 +19,22 @@ test.describe("Navigation Panel", () => {
         await expect(anchorElement).toHaveText(name);
       }
     });
+
+    
+    test("should have correct styling", async ({ page }) => {
+      await page.evaluate(async () => {
+        await document.fonts.ready
+      })
+
+      const element = page.getByTestId("nav-home")
+      
+      const fontFamily = await element.evaluate((el) => window.getComputedStyle(el).fontFamily)
+      const computedColor = await element.evaluate((el) => window.getComputedStyle(el).color)
+
+      const expectedRgb = formatRgb(oklch(computedColor))
+      
+      expect(fontFamily.toLowerCase()).toContain("inter")
+      expect(expectedRgb).toBe("rgb(124, 207, 0)")
+    })
   });
 });
