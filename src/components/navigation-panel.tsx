fe0cacd8f69758d125d8a658a4b29afca3e75c84
Dan G <daniel.w.gray1994@gmail.com>
2025-04-24 08:22:15 -0400

    added light and dark versions of foreground color

--- src/components/navigation-panel.tsx
+++ src/components/navigation-panel.tsx
@@ -12,7 +12,7 @@ export default function NavigationPanel() {
   return (
     <aside
       data-testid="navigation-panel"
-      className={`flex flex-col fixed top-0 left-0 h-full w-1/5 bg-foreground p-2 transform transition-transform duration-300  ${
+      className={`flex flex-col fixed top-0 left-0 h-full w-1/5 bg-foreground-dark p-2 transform transition-transform duration-300  ${
         open
           ? "translate-x-0 shadow-[4px_0_10px_rgba(0,0,0,0.6)]"
           : "-translate-x-5/6 shadow-[4px_0_10px_rgba(0,0,0,0.4)]"
