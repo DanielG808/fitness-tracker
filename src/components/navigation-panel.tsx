3f35e33b5a01370f07e2025813361eea2a390a03
Dan G <daniel.w.gray1994@gmail.com>
2025-04-24 09:45:40 -0400

    changed menu-button icon changed menubutton layout

--- src/components/navigation-panel.tsx
+++ src/components/navigation-panel.tsx
@@ -14,11 +14,13 @@ export default function NavigationPanel() {
       data-testid="navigation-panel"
       className={`flex flex-col bg-foreground-dark transition-all duration-300 overflow-hidden ${
         open
-          ? "w-1/5 min-w[10rem] p-2 shadow-[4px_0_10px_rgba(0,0,0,0.6)]"
+          ? "w-1/5 min-w[10rem] shadow-[4px_0_10px_rgba(0,0,0,0.6)]"
           : "w-0 min-w-0 p-0"
       }`}
     >
-      <MenuButton open={open} setOpen={setOpen} />
+      <div className="flex justify-center h-20">
+        <MenuButton open={open} setOpen={setOpen} />
+      </div>
       <div className="my-12">
         <Logo testId="nav-panel-logo" className="ml-5" />
         <NavLinkList />
