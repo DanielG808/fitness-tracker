4fb164c3a53055ee456442ba32d692621455ca5c
Dan G <daniel.w.gray1994@gmail.com>
2025-04-26 12:08:31 -0400

    made navigation-panel ui responsive

--- src/components/nav-link-list.tsx
+++ src/components/nav-link-list.tsx
@@ -4,7 +4,7 @@ import NavLink from "./nav-link";
 export default function NavLinkList() {
   return (
     <nav>
-      <ul className="flex flex-col mx-16 mt-10 space-y-5 text-xl transition-all">
+      <ul className="flex flex-col mx-16 mt-10 sm:mx-6 md:mx-12 xl:mx-16 space-y-5 text-xl transition-all">
         {navLinks.map(({ icon: Icon, name, path }) => (
           <NavLink
             testId={`nav-${path.replace(/\//g, "") || "home"}`}
--- src/components/navigation-panel.tsx
+++ src/components/navigation-panel.tsx
@@ -13,7 +13,10 @@ export default function NavigationPanel() {
       data-testid="navigation-panel"
       className={`flex flex-col bg-foreground-dark transition-all duration-300 overflow-hidden ${
         open
-          ? "w-screen h-screen sm:w-1/5 sm:min-w[10rem] sm:h-auto shadow-[4px_0_10px_rgba(0,0,0,0.6)]"
+          ? `w-screen h-screen 
+          sm:w-1/3 sm:min-w-[12rem] sm:h-auto
+          xl:w-1/5 xl:min-w-[10rem] 
+          shadow-[4px_0_10px_rgba(0,0,0,0.6)]`
           : "w-0 min-w-0 p-0"
       }`}
     >
