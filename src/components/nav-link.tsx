cd3e32aecb13cb99b34320d67ca11073a0237f35
Dan G <daniel.w.gray1994@gmail.com>
2025-04-26 13:07:43 -0400

    refactored nav-link to make whole area clickable

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -26,15 +26,14 @@ export default function NavLink({
   }
 
   return (
-    <li
-      data-testid={testId}
-      key={path}
-      onClick={handleClick}
-      className="flex items-center w-full h-16 px-3 space-x-4 rounded-full hover:bg-white/15 transition-all duration-300 cursor-pointer"
-    >
-      <Icon className="w-10 h-10 shrink-0" />
-      <Link className="whitespace-nowrap" href={path}>
-        {name}
+    <li data-testid={testId} key={path}>
+      <Link
+        onClick={handleClick}
+        className="flex items-center w-full h-16 px-3 space-x-4 rounded-full hover:bg-white/15 transition-all duration-300 cursor-pointer whitespace-nowrap"
+        href={path}
+      >
+        <Icon className="w-10 h-10 shrink-0" />
+        <span>{name}</span>
       </Link>
     </li>
   );
