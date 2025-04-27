dcf8dc4fda48c582f6781339ded2a46c71fd4370
Dan G <daniel.w.gray1994@gmail.com>
2025-04-27 14:22:15 -0400

    created an input component

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,7 +1,9 @@
+import Input from "./ui/input";
+
 export default function LoginForm() {
   return (
     <form className="flex justify-center items-center h-full w-full">
-      LoginForm
+      <Input />
     </form>
   );
 }
