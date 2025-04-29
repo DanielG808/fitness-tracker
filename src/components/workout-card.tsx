9988a736e73ab386fa7c2dbd89b48bf88d05fdf7
Dan Gray <dan.gray@nice.com>
2025-04-29 16:06:55 -0400

    added layout styling to workouts-list

---
+++ src/components/workout-card.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WorkoutCard() {
+  return (
+    <article className="bg-white w-3/5">WorkoutCard</article>
+  )
+}
--- src/components/workouts-list.tsx
+++ src/components/workouts-list.tsx
@@ -1,5 +1,11 @@
+import WorkoutCard from "./workout-card"
+
 export default function WorkoutsList() {
   return (
-    <ul>WorkoutsList</ul>
+    <ul className="flex flex-col items-center w-full space-y-4">
+      <WorkoutCard />
+      <WorkoutCard />
+      <WorkoutCard />
+    </ul>
   )
 }
