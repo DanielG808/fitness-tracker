d84456abfa2b7381d60c321e9733222706c7e117
Dan Gray <dan.gray@nice.com>
2025-04-30 18:55:41 -0400

    adjusted modal sizing

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -6,13 +6,13 @@ type ModalProps = {
 
 export default function Modal({ closeModal }: ModalProps) {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-
-        <section className="bg-white p-6 rounded-2xl relative">
-            <XButton onClick={closeModal} />
-            Modal
-            
-        </section>
+    <div className="fixed inset-0 z-50 bg-black/50">
+        <div className="flex items-center justify-center absolute inset-0">
+            <section className="bg-white w-3/5 p-6 rounded-2xl relative">
+                <XButton onClick={closeModal} />
+                Modal
+            </section>
+        </div>
     </div>
   )
 }
