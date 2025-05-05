9dcc0bae8848a66accd10e82b275ecdc07ff1b7b
Dan G <daniel.w.gray1994@gmail.com>
2025-05-05 16:59:32 -0400

    updated test for useInputList second parameter

--- src/components/new-workout-form.tsx
+++ src/components/new-workout-form.tsx
@@ -4,7 +4,10 @@ import Button from "./ui/button";
 import Form from "./ui/form";
 import Input from "./ui/input";
 import LineBreak from "./ui/line-break";
-import { inputMinimum, workoutFormInputs } from "@/lib/constants/workoutFormInputs";
+import {
+  inputMinimum,
+  workoutFormInputs,
+} from "@/lib/constants/workoutFormInputs";
 import { useInputList } from "@/lib/hooks/useInputList";
 
 type NewWorkoutFormProps = {
@@ -12,7 +15,10 @@ type NewWorkoutFormProps = {
 };
 
 export default function NewWorkoutForm({ closeModal }: NewWorkoutFormProps) {
-  const { inputs, addInput, removeInput } = useInputList(workoutFormInputs, inputMinimum);
+  const { inputs, addInput, removeInput } = useInputList(
+    workoutFormInputs,
+    inputMinimum
+  );
 
   return (
     <Form>
@@ -21,25 +27,23 @@ export default function NewWorkoutForm({ closeModal }: NewWorkoutFormProps) {
         <Input key={index} name={name} placeholder={placeholder} />
       ))}
       <div className="flex flex-col sm:flex-row space-x-2">
-
-      {/* Add/Remove exercise input buttons */}
-      <Button
-        onClick={() => addInput("Exercise", "Enter an exercise...")}
-        style="secondary"
-        className="text-sm h-8 my-2 w-40"
-      >
-        + Add exercise
-      </Button>
-      {
-        inputs.length > inputMinimum &&
+        {/* Add/Remove exercise input buttons */}
         <Button
+          onClick={() => addInput("Exercise", "Enter an exercise...")}
+          style="secondary"
+          className="text-sm h-8 my-2 w-40"
+        >
+          + Add exercise
+        </Button>
+        {inputs.length > inputMinimum && (
+          <Button
             onClick={() => removeInput()}
             style="secondary"
             className="text-sm h-8 my-2 w-40"
-        >
+          >
             - Remove exercise
-        </Button>
-      }
+          </Button>
+        )}
       </div>
 
       <LineBreak />
