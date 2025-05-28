26f7efae0b24b9023d5acd6e966168fe5c393a62
Dan G <daniel.w.gray1994@gmail.com>
2025-05-28 17:46:30 -0400

    added GET and POST requests to /api/workouts endpoint

--- src/app/api/workouts/route.ts
+++ src/app/api/workouts/route.ts
@@ -1,7 +1,25 @@
 import { NextResponse } from "next/server";
-import { getAllWorkouts } from "./controller";
+import { createWorkout, getAllWorkouts } from "./controller";
+import { workoutCreateSchema } from "@/lib/validations/workoutSchema";
 
 export async function GET() {
-  const workouts = await getAllWorkouts();
-  return NextResponse.json(workouts);
+  try {
+    const workouts = await getAllWorkouts();
+    return NextResponse.json(workouts);
+  } catch (error) {
+    console.error("GET /api/workouts error:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const workoutData = workoutCreateSchema.parse(body);
+    const newWorkout = await createWorkout(workoutData);
+    return NextResponse.json(newWorkout, { status: 201 });
+  } catch (error) {
+    console.error("POST /api/workouts error:", error);
+    return new NextResponse("Invalid Request Data", { status: 400 });
+  }
 }
