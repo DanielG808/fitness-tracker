8fe586190e0352f173ee1b0e2adc4ca5f3aabefd
Dan Gray <daniel.w.gray1994@gmail.com>
2025-05-29 08:58:44 -0400

    Resolve merge conflict in calendar-grid.tsx

--- src/components/calendar-grid.tsx
+++ src/components/calendar-grid.tsx
@@ -8,6 +8,7 @@ import {
 } from "date-fns";
 import React from "react";
 import DayCell from "./day-cell";
+import { cn } from "@/lib/utils/cn";
 
 export default function CalendarGrid() {
   const now = new Date();
@@ -19,19 +20,19 @@ export default function CalendarGrid() {
 
   return (
     <section>
-      <h2 className="text-xl font-bold text-center">
+      <h2 className="text-xl font-bold text-center my-2">
         {format(currentMonth, "MMMM yyyy")}
       </h2>
 
-      <div className="grid grid-cols-7 text-center font-medium text-gray-500">
-        {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day) => (
+      <div className="grid grid-cols-7 border border-t-0 border-black/15 text-center font-medium text-gray-500 my-1">
+        {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day, index) => (
           <div key={day}>{day}</div>
         ))}
       </div>
 
       <div className="grid grid-cols-7 gap-2 mt-2">
         {days.map((day) => (
-          <DayCell day={day} />
+          <DayCell key={day.toISOString()} day={day} />
         ))}
       </div>
     </section>
