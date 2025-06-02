2b7d6fded10be8c2917d9db35eee5cd362192b77
Dan Gray <daniel.w.gray1994@gmail.com>
2025-06-02 18:56:39 -0400

    refactored workout deletion into custom useWorkouts hook

--- src/components/workout-card.tsx
+++ src/components/workout-card.tsx
@@ -4,6 +4,7 @@ import { Workout } from "@/lib/validations/workoutSchema";
 import WorkoutDetails from "./workout-details";
 import XButton from "./x-button";
 import { useRouter } from "next/navigation";
+import { useWorkouts } from "@/lib/hooks/useWorkouts";
 
 type WorkoutCardProps = {
   workout: Workout;
@@ -11,27 +12,7 @@ type WorkoutCardProps = {
 
 export default function WorkoutCard({ workout }: WorkoutCardProps) {
   const { id, title, duration, exerciseList } = workout;
-  const router = useRouter();
-
-  async function handleDelete() {
-    try {
-      const response = await fetch(`/api/workouts/${id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-type": "application/json",
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-      }
-
-      console.log(response);
-      router.refresh();
-    } catch (error) {
-      console.error("Failed to delete workout:", error);
-    }
-  }
+  const { deleteWorkout } = useWorkouts();
 
   return (
     <li className="bg-white w-full md:w-2/3 h-28 p-4 rounded-lg shadow-md hover:shadow-xl duration-300">
@@ -42,7 +23,7 @@ export default function WorkoutCard({ workout }: WorkoutCardProps) {
           duration={duration}
           exerciseList={exerciseList}
         />
-        <XButton onClick={() => handleDelete()} />
+        <XButton onClick={() => deleteWorkout(id)} />
       </article>
     </li>
   );
