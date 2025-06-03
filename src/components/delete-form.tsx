c4ab8ab18c3f32b90685e344171136c446e7edc2
Dan G <daniel.w.gray1994@gmail.com>
2025-06-03 09:50:20 -0400

    delete button now closes the delete modal and sends the delete request

--- src/components/delete-form.tsx
+++ src/components/delete-form.tsx
@@ -1,6 +1,14 @@
+import { useWorkouts } from "@/lib/hooks/useWorkouts";
 import Button from "./ui/button";
 
 export default function DeleteForm({ id, title, closeModal }) {
+  const { deleteWorkout } = useWorkouts();
+
+  async function handleDelete(id: string) {
+    closeModal();
+    await deleteWorkout(id);
+  }
+
   return (
     <section className="flex flex-col justify-center items-center w-full">
       <h2 className="text-lg text-center">
@@ -8,7 +16,11 @@ export default function DeleteForm({ id, title, closeModal }) {
         <span className="font-bold">{title}</span> from your workouts list?
       </h2>
       <div className="flex justify-center items-center w-full mt-4 space-x-2">
-        <Button variant="warning" className="w-1/5">
+        <Button
+          onClick={() => handleDelete(id)}
+          variant="warning"
+          className="w-1/5"
+        >
           Delete
         </Button>
         <Button onClick={closeModal} variant="secondary" className="w-1/5">
