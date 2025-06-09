7be6eed8590e13b95887e8dcdc3968c06906733a
Dan Gray <daniel.w.gray1994@gmail.com>
2025-06-09 18:42:38 -0400

    passed errors object to error-message-container to render error message

--- src/components/error-message-container.tsx
+++ src/components/error-message-container.tsx
@@ -1,3 +1,17 @@
-export default function ErrorMessageContainer() {
-  return <div>ErrorMessageContainer</div>;
+import { FieldError, FieldErrors } from "react-hook-form";
+
+type ErrorMessageContainerProps = {
+  errors: FieldErrors;
+};
+
+export default function ErrorMessageContainer({
+  errors,
+}: ErrorMessageContainerProps) {
+  const titleError = errors?.title as FieldError | undefined;
+
+  return (
+    <div>
+      <p>{titleError?.message}</p>
+    </div>
+  );
 }
