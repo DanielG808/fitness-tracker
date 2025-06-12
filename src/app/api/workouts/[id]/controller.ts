7b6e1cdcf04486bb44e03d9cc261523860a39432
Dan Gray <daniel.w.gray1994@gmail.com>
2025-06-12 18:07:08 -0400

    implemented PUT request for workout model

--- src/app/api/workouts/[id]/controller.ts
+++ src/app/api/workouts/[id]/controller.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/db/prisma";
 import { WorkoutCreate } from "@/lib/validations/workoutSchema";
+import { Prisma } from "../../../../../prisma/prisma-client";
 
 export async function getWorkout(id: string) {
   return prisma.workout.findUnique({
@@ -9,11 +10,47 @@ export async function getWorkout(id: string) {
 }
 
 export async function updateWorkout(workoutId: string, data: WorkoutCreate) {
-  const existingIds = await prisma.exercise.findMany({
-    where: { workoutId },
-  });
+  try {
+    const updatedWorkout = await prisma.$transaction(async (tx) => {
+      await tx.workout.update({
+        where: { id: workoutId },
+        data: {
+          title: data.title,
+          duration: data.duration,
+          updatedAt: new Date(),
+        },
+      });
 
-  return existingIds;
+      await tx.exercise.deleteMany({
+        where: { workoutId },
+      });
+
+      await tx.exercise.createMany({
+        data: data.exerciseList.map((exercise) => ({
+          ...exercise,
+          reps: exercise.reps ?? null,
+          workoutId,
+        })),
+      });
+
+      const fullWorkout = await tx.workout.findUnique({
+        where: { id: workoutId },
+        include: { exerciseList: true },
+      });
+
+      return fullWorkout;
+    });
+
+    return updatedWorkout;
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    ) {
+      throw new Error("WORKOUT_NOT_FOUND");
+    }
+    throw error;
+  }
 }
 
 export async function deleteWorkout(id: string) {
