6dfe6d6f265e1057055e700ccef611d112e36ccb
Dan Gray <daniel.w.gray1994@gmail.com>
2025-08-04 16:33:34 -0400

    added test case for handling undefined and NaN as 0

--- src/lib/utils/calculateDuration.test.ts
+++ src/lib/utils/calculateDuration.test.ts
@@ -1,3 +1,4 @@
+import { WorkoutCreate } from "../validations/workoutSchema";
 import { calculateDuration } from "./calculateDuration";
 
 describe("calculateDuration", () => {
@@ -7,5 +8,13 @@ describe("calculateDuration", () => {
       { name: "Pushup", minutes: 5, reps: undefined },
     ];
     expect(calculateDuration(list)).toBe(15);
+  });
+
+  it("treats missing or invalid minutes as 0", () => {
+    const list = [
+      { name: "Squat", minutes: undefined, reps: undefined },
+      { name: "Pushup", minutes: NaN, reps: undefined },
+    ] as unknown as WorkoutCreate["exerciseList"];
+    expect(calculateDuration(list)).toBe(0);
   });
 });
