5c96eb76e735c823a6830055f48d93bcbdab9080
Dan Gray <daniel.w.gray1994@gmail.com>
2025-08-05 11:44:37 -0400

    added test case for 2 errors

--- src/lib/utils/getFirstExerciseErrors.test.ts
+++ src/lib/utils/getFirstExerciseErrors.test.ts
@@ -1,8 +1,25 @@
+import { FieldError, FieldErrors } from "react-hook-form";
 import { getFirstExerciseErrors } from "./getFirstExerciseErrors";
+import { WorkoutCreate } from "../validations/workoutSchema";
 
 describe("getFirstExerciseErrors", () => {
   it("returns nulls if input is not an array", () => {
     const result = getFirstExerciseErrors(undefined);
     expect(result).toEqual({ name: null, minutes: null });
+  });
+
+  it("returns the first error messages for name and the raw object for minutes", () => {
+    const mockErrors = [
+      {
+        name: { message: "Name is required." } as FieldError,
+        minutes: { message: "Minutes must be a number." } as FieldError,
+      },
+    ] as unknown as NonNullable<FieldErrors<WorkoutCreate>["exerciseList"]>;
+
+    const result = getFirstExerciseErrors(mockErrors);
+    expect(result).toEqual({
+      name: "Name is required.",
+      minutes: { message: "Minutes must be a number." },
+    });
   });
 });
