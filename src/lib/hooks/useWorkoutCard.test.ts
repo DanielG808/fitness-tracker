4ba46a28c238b361aa4fd7543218e8f99d6cd5c1
Dan Gray <daniel.w.gray1994@gmail.com>
2025-08-07 11:10:10 -0400

    added test case testing toggle functionality

--- src/lib/hooks/useWorkoutCard.test.ts
+++ src/lib/hooks/useWorkoutCard.test.ts
@@ -1,9 +1,19 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useWorkoutCard } from "./useWorkoutCard";
 
 describe("useWorkoutCard", () => {
   it("should intialize with a value of false", () => {
     const { result } = renderHook(() => useWorkoutCard());
     expect(result.current.expanded).toEqual(false);
+  });
+
+  it("should toggle value to true when toggleWorkoutCard is called", () => {
+    const { result } = renderHook(() => useWorkoutCard());
+
+    act(() => {
+      result.current.toggleWorkoutCard();
+    });
+
+    expect(result.current.expanded).toEqual(true);
   });
 });
