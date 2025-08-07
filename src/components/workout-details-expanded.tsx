aa2eca23da447b98499e762d04cb9f0614f9a2d3
Dan Gray <daniel.w.gray1994@gmail.com>
2025-08-07 09:25:20 -0400

    styled workout-details-expanded component

--- src/components/workout-details-expanded.tsx
+++ src/components/workout-details-expanded.tsx
@@ -8,12 +8,20 @@ export default function WorkoutDetailsExpanded({
   exerciseList,
 }: WorkoutDetailsExpandedProps) {
   return (
-    <div className="text-sm text-gray-600">
+    <div className="flex space-x-10 text-sm text-gray-600">
       {exerciseList.map((exercise, index) => (
-        <div key={index} className="flex">
-          <p>{exercise.name}:</p>
-          <p>{`${exercise.minutes} minutes`}</p>
-          {exercise.reps && <p>{`${exercise.reps} reps`}</p>}
+        <div key={index} className="flex flex-col space-x-1 my-2">
+          <p className="font-semibold">{exercise.name}:</p>
+          <div className="flex flex-col space-x-1 ml-2 py-1">
+            <p>
+              <span className="font-semibold">{exercise.minutes} </span>minutes
+            </p>
+            {exercise.reps && (
+              <p>
+                <span className="font-semibold">{exercise.reps} </span>reps
+              </p>
+            )}
+          </div>
         </div>
       ))}
     </div>
