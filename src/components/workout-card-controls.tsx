e9a425d0fd93f12af068f9138bdc52f2dc08a9c7
Dan Gray <daniel.w.gray1994@gmail.com>
2025-08-15 15:21:31 -0400

    changed icon on workout-card details button

--- src/components/workout-card-controls.tsx
+++ src/components/workout-card-controls.tsx
@@ -1,12 +1,12 @@
 import {
-  ArrowsPointingInIcon,
-  ArrowsPointingOutIcon,
+  ChevronDoubleUpIcon,
   PencilSquareIcon,
 } from "@heroicons/react/16/solid";
 import DeleteModal from "./delete-modal";
 import Button from "./ui/button";
 import WorkoutModal from "./workout-modal";
 import { Workout } from "@/lib/validations/workoutSchema";
+import { motion } from "framer-motion";
 
 type WorkoutCardControlsProps = {
   workout: Workout;
@@ -23,13 +23,6 @@ export default function WorkoutCardControls({
 
   return (
     <section className="flex flex-col space-y-3">
-      <Button variant="icon" onClick={onExpand}>
-        {!expanded ? (
-          <ArrowsPointingOutIcon className="h-5 w-5" />
-        ) : (
-          <ArrowsPointingInIcon className="h-5 w-5" />
-        )}
-      </Button>
       <WorkoutModal
         workout={workout}
         action="edit"
@@ -37,6 +30,14 @@ export default function WorkoutCardControls({
         buttonVariant="icon"
       />
       <DeleteModal id={id} title={title} />
+      <Button variant="icon" onClick={onExpand}>
+        <motion.div
+          animate={{ rotate: expanded ? 180 : 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <ChevronDoubleUpIcon className="h-5 w-5" />
+        </motion.div>
+      </Button>
     </section>
   );
 }
